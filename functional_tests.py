315288dd850bb7cf1fc6949f9a1edf32845a88ba
mohsen <mohsen1076@gmail.com>
2022-03-04 11:43:04 +0330

    Text for first user-story (adding new category) added.)

---
+++ functional_tests.py
@@ -0,0 +1,34 @@
+
+# Ali just heared about a cool site that it's users can add and organize
+# whatever they have learned in any field.
+# He goes to check that so, he opens the home page of the site:
+
+##############################################################################
+#################### User Story 1: Adding new category #######################
+# He sees that home page title & header contain:
+#   "Enter what you have learned"
+
+# In the home page he sees a button with a text that says:
+#   "add new category"
+
+# He clicks on that button and the site redirected to a new page:
+#   "adding new category" page
+
+# In the new page, he notices that the page title and header contain:
+#   "New Category"
+
+# In that page he can see an input box with a palaceholder text: 
+#   "enter new category"
+
+# He enters "programming stuffs" in the input box and hits Enter key.
+
+# The site redirects to a new page:
+#   "list of added categories" page
+# Ali notices that title and header of the new page contain:
+#   "list of your categories"
+
+# He can see '1: programming stuffs' text in the list
+
+# He is happy now and goes home for next user story
+##############################################################################
+

39f55816619d4d0fc4ce0e5e0a87ac2c47b04007
mohsen <mohsen1076@gmail.com>
2022-03-04 19:40:42 +0330

    An unit added to test if home page uses correct html.

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class HomePageTest(TestCase):
+    
+    def test_view_uses_correct_html(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, "posts/home.html")

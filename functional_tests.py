dcf53bb3aaa8d977637cbf6de036abe6ba6518c4
mohsen <mohsen1076@gmail.com>
2022-03-04 19:13:55 +0330

    Code for first user-story (adding new category) added.

--- functional_tests.py
+++ functional_tests.py
@@ -1,34 +1,73 @@
 
-# Ali just heared about a cool site that it's users can add and organize
-# whatever they have learned in any field.
-# He goes to check that so, he opens the home page of the site:
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from django.test import LiveServerTestCase
 
 ##############################################################################
-#################### User Story 1: Adding new category #######################
-# He sees that home page title & header contain:
-#   "Enter what you have learned"
+class NewVisitorTest(LiveServerTestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_add_a_new_category(self):
+        #################### User Story 1: Adding new category ###############
+        # Ali just heared about a cool site that it's users can add and
+        # organize whatever they have learned in any field.
+        # He goes to check that so, he opens the home page of the site:
+        self.browser.get(self.live_server_url)
 
-# In the home page he sees a button with a text that says:
-#   "add new category"
+        # He sees that home page title & header contain:
+        #   "Record what you have learned"
+        self.assertEqual(self.browser.title, "Record what you have learned")
+        main_header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertEqual(main_header_text, "Record what you have learned")
 
-# He clicks on that button and the site redirected to a new page:
-#   "adding new category" page
+        # In the home page he sees a button with a text that says:
+        #   "add new category"
+        add_new_category_link = self.browser.find_element(By.ID, "add-new-category-link")
+        self.assertEqual(add_new_category_link.text, "add new category")
 
-# In the new page, he notices that the page title and header contain:
-#   "New Category"
+        # He clicks on that button and the site redirected to a new page:
+        #   "adding new category" page
+        add_new_category_link.click()
 
-# In that page he can see an input box with a palaceholder text: 
-#   "enter new category"
+        # In the new page, he notices that the page title and header contain:
+        #   "New Category"
+        self.assertEqual(self.browser.title, "New Category")
+        main_header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertEqual(main_header_text, "New Category")
 
-# He enters "programming stuffs" in the input box and hits Enter key.
+        # In that page he can see an input box with a palaceholder text: 
+        #   "Enter a new category"
+        input_box = self.browser.find_element(By.ID, "new-category-input")
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            "Enter a new category"
+        )
 
-# The site redirects to a new page:
-#   "list of added categories" page
-# Ali notices that title and header of the new page contain:
-#   "list of your categories"
+        # He enters "programming stuffs" in the input box and hits Enter key.
+        input_box.send_keys("programming stuffs")
+        input_box.send_keys(Keys.ENTER)
 
-# He can see '1: programming stuffs' text in the list
+        # The site redirects to a new page:
+        #   "list of added categories" page
+        # Ali notices that title and header of the new page contain:
+        #   "list of your categories"
+        self.assertEqual(self.browser.title, "list of your categories")
+        main_header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertEqual(main_header_text, "list of your categories")
 
-# He is happy now and goes home for next user story
-##############################################################################
+        # He can see that the page has listed his new added category:
+        # '1: programming stuffs'
+        categories_table = self.browser.find_element(By.ID, "categories-table")
+        rows = categories_table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn("1: programming stuffs", [row.text for row in rows])
+
+        # He is happy now and goes home for next user story
+        ######################################################################
 

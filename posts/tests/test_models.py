d4a751fb9ac5b6831952258f99200168742779f3
mohsen <mohsen1076@gmail.com>
2022-03-15 18:27:39 +0330

    Refactoring: creating a directory for tests in posts app and splitting test for views and test for models.

---
+++ posts/tests/test_models.py
@@ -0,0 +1,33 @@
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+from posts.models import Category
+
+
+class CategoryModelTest(TestCase):
+    
+    def test_saving_and_retrieving_category_objects(self):
+        first_category = Category()
+        first_category.title = "Programming"
+        first_category.save()
+
+        second_category = Category()
+        second_category.title = "English"
+        second_category.save()
+
+        saved_categories = Category.objects.all()
+        self.assertEqual(saved_categories.count(), 2)
+        self.assertEqual(saved_categories[0].title, "Programming")
+        self.assertEqual(saved_categories[1].title, "English")
+    
+    def test_can_not_save_category_with_empty_title(self):
+        category = Category(title='')
+        with self.assertRaises(ValidationError):
+            category.save()
+            category.full_clean()
+    
+    def test_can_not_save_multiple_categoris_with_same_name(self):
+        Category.objects.create(title="some title")
+        with self.assertRaises(ValidationError):
+            second_category = Category.objects.create(title= "some title")
+            second_category.full_clean()
--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.utils.html import escape
 
@@ -89,8 +88,6 @@ class AddNewCategoryPageTest(TestCase):
         self.assertEqual(Category.objects.all().count(), 1)
 
 
-
-
 class CategoryListPageTest(TestCase):
 
     def test_view_uses_correct_html(self):
@@ -105,32 +102,3 @@ class CategoryListPageTest(TestCase):
 
         self.assertContains(response, "Programming")
         self.assertContains(response, "English")
-
-
-class CategoryModelTest(TestCase):
-    
-    def test_saving_and_retrieving_category_objects(self):
-        first_category = Category()
-        first_category.title = "Programming"
-        first_category.save()
-
-        second_category = Category()
-        second_category.title = "English"
-        second_category.save()
-
-        saved_categories = Category.objects.all()
-        self.assertEqual(saved_categories.count(), 2)
-        self.assertEqual(saved_categories[0].title, "Programming")
-        self.assertEqual(saved_categories[1].title, "English")
-    
-    def test_can_not_save_category_with_empty_title(self):
-        category = Category(title='')
-        with self.assertRaises(ValidationError):
-            category.save()
-            category.full_clean()
-    
-    def test_can_not_save_multiple_categoris_with_same_name(self):
-        Category.objects.create(title="some title")
-        with self.assertRaises(ValidationError):
-            second_category = Category.objects.create(title= "some title")
-            second_category.full_clean()
